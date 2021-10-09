8f0ccefc8ea69858efd1d048e8984f5ed82458a4
George Mathias <gmathias80@gmail.com>
2021-10-08 23:33:04 -0300

    campos de texto, ajuste cypress.json

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress-xpath" />
 
 describe('Work with basic elements', () => {
-    
+
     before(() => {
         cy.visit("/cypress/componentes.html")
     });
@@ -17,15 +17,36 @@ describe('Work with basic elements', () => {
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     });
 
-    it.only('Links', () => {
+    it('Links', () => {
         cy.xpath('//a[contains(text(),"Voltar")]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
-        
+
         cy.reload()
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
+    });
+
+    it.only('TextFields', () => {
+        cy.get('#formNome').type('Cypress test')
+        cy.get('#formNome').should('have.value', 'Cypress test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 150})
+            .should('have.value', 'acerto')
     });
 
 });

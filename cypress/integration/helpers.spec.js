c49955dee589af270b2d7820bde35fd4d5aa6c37
George Mathias <gmathias80@gmail.com>
2021-10-10 14:52:59 -0300

    utilizando invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -26,7 +26,7 @@ describe('Helpers...', () => {
         }).should('be.equal', 2)
     });
 
-    it.only('Its...', () => {
+    it('Its...', () => {
         const obj = { nome: 'User', idade: 20}
         cy.wrap(obj).should('have.property', 'nome', 'User')
         cy.wrap(obj).its('nome').should('be.equal', 'User')
@@ -40,4 +40,18 @@ describe('Helpers...', () => {
         cy.visit("/cypress/componentes.html")
         cy.title().its('length').should('be.equal', 20)
     })
+
+    it.only('Invoke...', () => {
+        const getValue = () => 1
+        const soma = (a, b) => a + b
+
+        cy.wrap({fn: getValue}).invoke('fn').should('be.equal', 1)
+        cy.wrap({fn: soma}).invoke('fn', 2, 5).should('be.equal', 7)
+
+        cy.visit("/cypress/componentes.html")
+        cy.get('#formNome').invoke('val', 'Texto via invoke')
+        cy.window().invoke('alert', 'dá pra ver?')
+        cy.get('#resultado')
+            .invoke("html", '<input type="button" value="hacket!"/>')
+    });
 });

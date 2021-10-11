92980e24bb1280ced99c876c209e8ed800eb4fe5
George Mathias <gmathias80@gmail.com>
2021-10-10 21:19:11 -0300

    dívida: combos

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -84,12 +84,31 @@ describe('Work with basic elements', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('1graucomp')
             .should('have.value', '1graucomp')
+
+        cy.get('[data-test=dataEscolaridade] option')
+        .should('have.length', 8)
+        
+        cy.get('[data-test=dataEscolaridade] option').then($arr => {
+            const values = []
+            $arr.each(function () {
+                values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(["Superior", "Mestrado"])
+        })
+           
     });
 
-    it('Combo multiplo', () => {
+    it.only('Combo multiplo', () => {
         cy.get('[data-testid=dataEsportes]')
             .select(['natacao', 'Corrida', 'nada'])
 
-        //TODO validar opções selecionadas do combo multiplo
-    });
+        cy.get('[data-testid=dataEsportes]').then($el => {
+            expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
+        
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
+    })
 });

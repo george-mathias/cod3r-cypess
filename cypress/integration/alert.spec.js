42e3008e9b5ef63f57dcf87afd85922cda235397
George Mathias <gmathias80@gmail.com>
2021-10-13 22:20:17 -0300

    desafio: validar mensagens

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -51,7 +51,7 @@ describe('Alerts...', () => {
         cy.get('#confirm').click()
     })
     
-    it.only('Prompt', () => {
+    it('Prompt', () => {
 
         cy.window().then(win => {
             cy.stub(win, 'prompt').returns('42').as('prompt')
@@ -65,5 +65,28 @@ describe('Alerts...', () => {
         cy.get('#prompt').click()
 
     })
+    
+    it.only('Challenge', () => {
+        
+        const stub = cy.stub().as('alerta')
+
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+        cy.get('#formNome').type('Wagner')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+            
+        cy.get('[data-cy=dataSobrenome]').type('Aquino')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    })
+
 
 });

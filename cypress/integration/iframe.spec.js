50e02e7958fd7602a419c09415df85dd29ac7408
George Mathias <gmathias80@gmail.com>
2021-10-14 21:28:43 -0300

    iFrame, parte 2

--- cypress/integration/iframe.spec.js
+++ cypress/integration/iframe.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Work with iFrames', () => {
-    
+
     it('Deve preencher campo de texto', () => {
         cy.visit("/cypress/componentes.html")
         cy.get('#frame1').then(iframe => {
@@ -9,11 +9,17 @@ describe('Work with iFrames', () => {
             cy.wrap(body).find('#tfield')
                 .type('funciona?')
                 .should('have.value', 'funciona?')
-
-            cy.on('window:alert', msg => {
-                expect(msg).to.equal('Alert Simples')
-            })
-            cy.wrap(body).find('#otherButton').click()
         })
     })
+
+    it('Deve testar frame diretamente', () => {
+        cy.visit("/cypress/frame.html")
+        cy.get('#otherButton').click()
+
+
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!')
+        })
+    })
+
 });

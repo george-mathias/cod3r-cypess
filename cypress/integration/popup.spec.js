f74fb50a70ce0f22b5b5448842ed04c0f058b711
George Mathias <gmathias80@gmail.com>
2021-10-15 20:22:12 -0300

    popup

---
+++ cypress/integration/popup.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+
+describe('Work with Popup', () => {
+    
+    it('Deve testar frame diretamente', () => {
+        cy.visit("/cypress/frame.html")
+        cy.get('#otherButton').click()
+
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!')
+        })
+    })
+    
+    it.only('Deve verificar se o popup foi invocado', () => {
+        cy.visit("/cypress/componentes.html")
+        cy.window().then(win => {
+            cy.stub(win, 'open').as('winOpen')
+        })
+        cy.get('#buttonPopUp').click()
+        cy.get('@winOpen').should('be.called')
+    })
+
+
+});

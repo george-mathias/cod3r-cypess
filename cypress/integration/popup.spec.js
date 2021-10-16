92162fce0d5eb5b46d7eb87a3ba1bdbc9174f60a
George Mathias <gmathias80@gmail.com>
2021-10-15 21:12:50 -0300

    popup via links

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -11,7 +11,7 @@ describe('Work with Popup', () => {
         })
     })
     
-    it.only('Deve verificar se o popup foi invocado', () => {
+    it('Deve verificar se o popup foi invocado', () => {
         cy.visit("/cypress/componentes.html")
         cy.window().then(win => {
             cy.stub(win, 'open').as('winOpen')
@@ -21,4 +21,30 @@ describe('Work with Popup', () => {
     })
 
 
+    describe.only('With links...', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        });
+
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        });
+        
+        it('Should acdcess popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+        });
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+            cy.get('#tfield').type('funciona')
+        });
+    });
 });

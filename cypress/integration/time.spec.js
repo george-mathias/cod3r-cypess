30607953b0a6894821b7d92cd76684bbdfc3cc83
George Mathias <gmathias80@gmail.com>
2021-10-17 18:01:47 -0300

    ADD: tick

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Work with clock', () => {
-    
+
     before(() => {
         cy.visit("/cypress/componentes.html")
     });
@@ -9,14 +9,32 @@ describe('Work with clock', () => {
     it('Going back to the past', () => {
         // cy.get('#buttonNow').click()
         // cy.get('#resultado > span').should('contain','17/10/2021')
-        
+
         // cy.clock()
         // cy.get('#buttonNow').click()
         // cy.get('#resultado > span').should('contain','31/12/1969')
-        
+
         const dt = new Date(2012, 3, 10, 15, 23, 50)
         cy.clock(dt.getTime())
         cy.get('#buttonNow').click()
-        cy.get('#resultado > span').should('contain','10/04/2012')        
+        cy.get('#resultado > span').should('contain', '10/04/2012')
+    });
+
+    it.only('Goes to the future', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '16345')
+        cy.get('#resultado > span').invoke('text').should('gt', 1634502538641)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+        cy.wait(1000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 1000)
+        
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 5000)
     });
 });

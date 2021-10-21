993dcb851ac8d2f65f8502edc99e2ba02b517e5e
George Mathias <gmathias80@gmail.com>
2021-10-20 22:55:48 -0300

    ADD: Inserir conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -4,13 +4,20 @@ describe('Should test at a funcional level', () => {
     
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('.input-group > .form-control').type('george@gmail.com')
-        cy.get(':nth-child(2) > .form-control').type('abc123')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'Bem vindo,')
+        // cy.get('.input-group > .form-control').type('george@gmail.com')
+        // cy.get(':nth-child(2) > .form-control').type('abc123')
+        // cy.get('.btn').click()
+        // cy.get('.toast-message').should('contain', 'Bem vindo,')
     });
 
-    it('...', () => {
-
+    it('Should a create an account', () => {
+        cy.get('.dropdown-toggle > .fas').click()
+        cy.get('[href="/contas"]').click()
+        cy.url().should('contain', '/contas')
+        
+        cy.get('.form-control').type('mercado semanal')
+        cy.get('.btn').click()
+        cy.url().should('contain', '/contas')
+        cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
     });
 });

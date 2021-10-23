0f34f3f7e2653c43584ca82399271548f6b43d43
George Mathias <gmathias80@gmail.com>
2021-10-23 17:24:43 -0300

    ADD: alterar conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,13 +1,14 @@
-/// <reference types="Cypress" />
+/// <reference types="Cypress-xpath" />
 
 describe('Should test at a funcional level', () => {
     
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        // cy.get('.input-group > .form-control').type('george@gmail.com')
-        // cy.get(':nth-child(2) > .form-control').type('abc123')
-        // cy.get('.btn').click()
+        cy.get('.input-group > .form-control').type('george@gmail.com')
+        cy.get(':nth-child(2) > .form-control').type('abc123')
+        cy.get('.btn').click()
         // cy.get('.toast-message').should('contain', 'Bem vindo,')
+        // cy.get('[href="/reset"]') 
     });
 
     it('Should a create an account', () => {
@@ -15,9 +16,20 @@ describe('Should test at a funcional level', () => {
         cy.get('[href="/contas"]').click()
         cy.url().should('contain', '/contas')
         
-        cy.get('.form-control').type('mercado semanal')
+        cy.get('.form-control').type('Conta de teste')
         cy.get('.btn').click()
-        cy.url().should('contain', '/contas')
         cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    });
+
+    it('should update an account', () => {
+        // cy.get('[data-test=menu-settings').click()
+        // cy.get('[href=\'/contas').click()
+        cy.xpath("//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']").click()
+        cy.get('[data-test=nome]')
+            .clear()
+            .type('Conta alterada')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta atualizada')
+  
     });
 });

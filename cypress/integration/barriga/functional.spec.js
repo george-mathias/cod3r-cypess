0e7ee091795922e12433ae47f62dc17d7ea1b8d3
George Mathias <gmathias80@gmail.com>
2021-10-24 11:29:42 -0300

    ADD: inserir movimentação

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -32,4 +32,18 @@ describe('Should test at a funcional level', () => {
         cy.inserirConta('Conta alterada')
         cy.get(loc.MESSAGE).should('contain', 'code 400')
     });
+    
+    it('should create a transaction', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type(123)
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        // cy.get(loc.MENU.EXTRATO).click()
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('contain', '123')
+    });
 });

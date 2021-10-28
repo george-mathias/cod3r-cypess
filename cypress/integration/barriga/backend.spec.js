9237ebd6b566bf7dd946b8c55942bfc7f62afdc6
George Mathias <gmathias80@gmail.com>
2021-10-27 23:10:48 -0300

    ADD: inserir conta

---
+++ cypress/integration/barriga/backend.spec.js
@@ -0,0 +1,72 @@
+/// <reference types="Cypress-xpath" />
+
+describe('Should test at a funcional level', () => {
+    
+    before(() => {
+        // cy.login('george@gmail.com', 'abc123')
+    });
+    
+    beforeEach(() => {
+        // cy.resetConta()
+    });
+
+    it.only('Should a create an account', () => {
+       cy.request({
+           method: 'POST',
+           url: 'https://barrigarest.wcaquino.me/signin',
+           body: {
+               email: 'george@gmail.com',
+               redireciona: false,
+               senha: 'abc123'
+           }
+       }).its('body.token').should('not.be.empty')
+       .then(token => {
+
+            // esta opção não foi passada pelo instrutor
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/reset',
+                method: 'GET',
+                headers: { Authorization: `JWT ${token}` }
+            })
+
+           cy.request({
+               url: 'https://barrigarest.wcaquino.me/contas',
+               method: 'POST',
+               headers: {
+                   Authorization: `JWT ${token}`
+               },
+               body: {
+                   nome: 'Conta via rest'
+                   
+               }
+           }).as('response')
+       })
+
+       cy.get('@response').then(res => {
+           expect(res.status).to.be.equal(201)
+           expect(res.body).to.have.property('id')
+           expect(res.body).to.have.property('nome', 'Conta via rest')
+       })
+
+    });
+
+    it('should update an account', () => {
+       
+    });
+
+    it('should not cerate an account with same name', () => {
+       
+    });
+    
+    it('should create a transaction', () => {
+       
+    });
+
+    it('should get balance', () => {
+        
+    });
+
+    it('should remove a transaction', () => {
+        
+    });
+});
